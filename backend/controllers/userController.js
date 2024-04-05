4fafc70289e5cfd18b76f8276f69dcda6a1cb99e
BHANDARI <S2119656@glos.ac.uk>
2024-04-05 13:52:35 +0100

    logout and destroy cookie done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,7 +39,12 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 const logoutUser = asyncHandler(async(req, res)=>{
-    res.status(200).json({message: 'user logged out'})
+
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({message: 'User logged out successfully'});
 })
 
 const getUserProfile = asyncHandler(async(req, res)=>{
