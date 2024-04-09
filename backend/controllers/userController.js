65a9dc7349d49636ee6bd8e028a08ef2f2559b1f
Pawan <pawanb78@gmail.com>
2024-04-09 22:52:08 +0100

    update usser done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,16 +29,30 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 });
 
-
 const logoutUser = asyncHandler(async(req, res)=>{
     res.status(200).json({message: 'user logged out'})
 })
-
 const getUserProfile = asyncHandler(async(req, res)=>{
     res.status(200).json({message: 'User profile'})
 })
-
 const updateUserProfile = asyncHandler(async(req, res)=>{
+    const user = await User.findByID(req.user._id)
+    if(user){
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if(req.body.password){
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save()
+        res.status(200).json({_id: updatedUser._id, 
+        name: updatedUser.name,
+        email: updatedUser.email
+
+        })
+    }
+
     res.status(200).json({message: 'profile Updated'})
 })
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,3 +30,9 @@ userSchema.pre('save', async function(next){
 const User = mongoose.model('User', userSchema);
 
 export default User;
+
+
+
+
+
+
